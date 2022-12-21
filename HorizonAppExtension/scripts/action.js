7b016d0eb8bbcae12ef1f151cb3b970baf5fc606
for-shariq <forshariq@gmail.com>
2022-12-21 20:22:40 +0530

    init

---
+++ HorizonAppExtension/scripts/action.js
@@ -0,0 +1,36 @@
+
+document.getElementById('fillForm').addEventListener('click',function(){
+    //const tabId = getTabId();
+    var uid = document.getElementById('uid').value;
+
+    chrome.tabs.query({ active: true }, function (tabs) {
+      tab = tabs[0];  
+
+      new Promise(resolve=>{
+        chrome.scripting.executeScript({
+          target: { tabId:  tab.id, allFrames: true},
+          files: ['scripts/injector.js'],
+          args: [uid]
+        });    
+      }, () => { resolve(true) ;});
+      
+    });   
+    
+
+
+   /* chrome.tabs.query({ active: true }, function (tabs) {
+        let tab = tabs[0];
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tab.id },
+            files: ['./scripts/injector.js']            
+          }
+        );
+      });
+  /*  chrome.scripting.executeScript({
+        target: {allFrames: true},
+        files: ['injector.js'],
+        args: [value]    
+    });*/
+});
+
---
+++ HorizonAppExtension/scripts/injector.js
@@ -0,0 +1,21 @@
+var jsonData = '[{"Id": 1,"HorizonUserName": "shariq","HorizonPassword": "abc","IXLUserName": "arsalaanali","IXLPassword": "ayaanali","Active": 1,"SchoolId": 1},{"Id": 2,"HorizonUserName": "nasir","HorizonPassword": "abc","IXLUserName": "arsalaanali",		"IXLPassword": "ayaanali","Active": 1,"SchoolId": 1	},{	"Id": 3,	"HorizonUserName": "shariq",	"HorizonPassword": "abc",	"IXLUserName": "arsalaanali","IXLPassword": "ayaanali",	"Active": 0,"SchoolId": 2 }]'
+
+
+var username = document.querySelector('input[name="username"]');
+fillField(username,'shariq');
+var password = document.querySelector('input[name="password"]');
+fillField(password,'abc');
+
+function fillField(field, value) {
+    if(field){
+        field.value = value;
+    }
+}
+
+/*const script = document.createElement('script');
+script.src = 'background.js';
+script.onload = () => {
+    console.log('Script loaded successfuly');
+  };
+document.body.insertAdjacentHTML('beforeend', '<input type="button" text="click" onClick="clicked()">');
+*/
